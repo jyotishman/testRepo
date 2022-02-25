cb5b35c88bc26ea8e87840193645634791cadf62
Jyotishman Saikia <jyotishman.saikia@dotpe.in>
2022-02-25 10:56:30 +0530

    added redux logger

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import catalogReducer from './catalogSlice';
+import logger from 'redux-logger';
 
 const store = configureStore({
   reducer: {
@@ -8,6 +9,8 @@ const store = configureStore({
     catalogReducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({thunk: false, serializableCheck: false}),
+    getDefaultMiddleware({thunk: false, serializableCheck: false}).concat(
+      logger,
+    ),
 });
 export default store;
